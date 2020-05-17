822fbd5f9d452c66c40d4d6539bc1f575394f3fc
Javier Luna molina <javierlunamolina@gmail.com>
2020-05-17 23:17:02 +0200

    Add config mapping to retrieve docker-compose binary location

--- sardine/config.py
+++ sardine/config.py
@@ -5,3 +5,5 @@ _user = os.path.expanduser("~")
 SARDINE_FOLDER_PATH = os.getenv("SARDINE_FOLDER_PATH", os.path.join(_user, ".sardine"))
 
 SARDINE_RC_PATH = os.getenv("SARDINE_RC_PATH", os.path.join(_user, ".config", "sardine", ".sardinerc"))
+
+DOCKER_COMPOSE_EXECUTABLE_PATH = os.getenv('SARDINE_DOCKER_COMPOSE_EXECUTABLE_PATH', 'docker-compose')
