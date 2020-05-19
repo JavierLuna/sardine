b7aeeef7b208cf5f73298c43d5161f266d8d32a1
Javier Luna molina <javierlunamolina@gmail.com>
2020-05-19 20:26:08 +0200

    Fix typo in CLI -d option

--- sardine/cli/main.py
+++ sardine/cli/main.py
@@ -13,8 +13,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=('run', 'stop'), help="Command to run.")
     parser.add_argument("stack", help="Target stack alias.")
-    parser.add_argument("-d", "--deattached", action='store_true', default=False,
-                        help="Run the command in deattached mode.")
+    parser.add_argument("-d", "--detached", action='store_true', default=False,
+                        help="Run the command in detached mode.")
     args = parser.parse_args()
 
     command = commands[args.command]
